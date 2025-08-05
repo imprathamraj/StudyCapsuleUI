2c5b66568089f7bb16682c9737d8d8188fe205e4
Pratham Raj <rjpratham04@gmail.com>
2025-08-06 04:06:57 +0530

    [Pratham Raj]. Update app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Body } from "./body/body";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Body, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
